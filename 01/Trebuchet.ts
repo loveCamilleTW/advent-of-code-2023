fa6e071513027e6bafbcc6111e21522372f3b4ef
loveCamilleTW <yoyopetergod@gmail.com>
2023-12-11 23:05:54 +0800

    Feature: solve question 1

---
+++ 01/Trebuchet.ts
@@ -0,0 +1,19 @@
+import { readFileSync } from 'fs';
+
+function trebuchet(str: string): number {
+    const digits = str.match(/\d/g);
+    if (!digits) return 0;
+
+    const first = digits[0];
+    const last = digits[digits?.length - 1];
+
+    return Number(`${first}${last}`);
+}
+
+const input = readFileSync('input', 'utf-8');
+
+const ans = input.split('\n')
+    .map((str) => trebuchet(str))
+    .reduce((acc, cur) => acc + cur);
+
+console.log(ans);
