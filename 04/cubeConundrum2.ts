cab355077e3b2324f25b1913451e2cb40cf5a5ba
Peter_Lo <peter_lo@cmoney.com.tw>
2023-12-12 11:42:14 +0800

    Feature: solve question 4

---
+++ 04/cubeConundrum2.ts
@@ -0,0 +1,34 @@
+import { readFileSync } from "fs";
+import { parseGameRecordFromString, isColor } from "../03/cubeConundrum";
+
+function cubeConundrum2() {
+  const input = readFileSync("input.txt", "utf-8");
+  let sum = 0;
+
+  const gameRecords = input
+    .split("\n")
+    .map((str) => parseGameRecordFromString(str));
+
+  gameRecords.forEach((gameRecord) => {
+    const minCounts = {
+      red: 0,
+      blue: 0,
+      green: 0,
+    };
+
+    gameRecord.records.forEach((record) => {
+      Object.entries(record).forEach(([key, value]) => {
+        if (isColor(key)) {
+          minCounts[key] = minCounts[key] < value ? value : minCounts[key];
+        }
+      });
+    });
+
+    sum += minCounts["red"] * minCounts["blue"] * minCounts["green"];
+  });
+
+  return sum;
+}
+
+const ans = cubeConundrum2();
+console.log(ans);
