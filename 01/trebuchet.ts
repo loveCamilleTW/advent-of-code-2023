d3194ab39a9f4cb97c0ef689520f6cdb983d1e41
Peter_Lo <peter_lo@cmoney.com.tw>
2023-12-12 10:46:41 +0800

    Refactor: lint by prettier

--- 01/trebuchet.ts
+++ 01/trebuchet.ts
@@ -1,19 +1,20 @@
-import { readFileSync } from 'fs';
+import { readFileSync } from "fs";
 
 function trebuchet(str: string): number {
-    const digits = str.match(/\d/g);
-    if (!digits) return 0;
+  const digits = str.match(/\d/g);
+  if (!digits) return 0;
 
-    const first = digits[0];
-    const last = digits[digits?.length - 1];
+  const first = digits[0];
+  const last = digits[digits?.length - 1];
 
-    return Number(`${first}${last}`);
+  return Number(`${first}${last}`);
 }
 
-const input = readFileSync('input.txt', 'utf-8');
+const input = readFileSync("input.txt", "utf-8");
 
-const ans = input.split('\n')
-    .map((str) => trebuchet(str))
-    .reduce((acc, cur) => acc + cur);
+const ans = input
+  .split("\n")
+  .map((str) => trebuchet(str))
+  .reduce((acc, cur) => acc + cur);
 
 console.log(ans);
--- 02/calibration.ts
+++ 02/calibration.ts
@@ -1,65 +1,74 @@
-const DIGIT_WORDS: { [key: string]: number } = {
-    'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
-    'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'zero': 0
-}
+import { readFileSync } from "fs";
 
-import { readFileSync } from 'fs';
+const DIGIT_WORDS: { [key: string]: number } = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+  zero: 0,
+};
 
 function calibration(str: string): number {
-    let firstDigit: string | number | undefined;
-    let lastDigit: string | number | undefined;
-    let firstDigitIndex = Number.MAX_SAFE_INTEGER;
-    let lastDigitIndex = -1;
+  let firstDigit: string | number | undefined;
+  let lastDigit: string | number | undefined;
+  let firstDigitIndex = Number.MAX_SAFE_INTEGER;
+  let lastDigitIndex = -1;
 
-    Object.keys(DIGIT_WORDS).forEach(key => {
-        const index = str.indexOf(key);
+  Object.keys(DIGIT_WORDS).forEach((key) => {
+    const index = str.indexOf(key);
 
-        if (index === -1) return;
+    if (index === -1) return;
 
-        if (index < firstDigitIndex) {
-            firstDigitIndex = index;
-            firstDigit = DIGIT_WORDS[key];
-        }
-    })
-
-    Object.keys(DIGIT_WORDS).forEach(key => {
-        const index = str.lastIndexOf(key);
-
-        if (index === -1) return;
-
-        if (index > lastDigitIndex) {
-            lastDigitIndex = index;
-            lastDigit = DIGIT_WORDS[key];
-        }
-    })
-
-    for (let i = 0; i < firstDigitIndex && i < str.length; i++) {
-        const char = str[i];
-        if (isDigit(char)) {
-            firstDigit = char;
-            break;
-        }
+    if (index < firstDigitIndex) {
+      firstDigitIndex = index;
+      firstDigit = DIGIT_WORDS[key];
     }
+  });
 
-    for (let i = str.length - 1; i > lastDigitIndex && i >= 0; i--) {
-        const char = str[i];
-        if (isDigit(char)) {
-            lastDigit = char;
-            break;
-        }
+  Object.keys(DIGIT_WORDS).forEach((key) => {
+    const index = str.lastIndexOf(key);
+
+    if (index === -1) return;
+
+    if (index > lastDigitIndex) {
+      lastDigitIndex = index;
+      lastDigit = DIGIT_WORDS[key];
     }
+  });
 
-    return Number(`${firstDigit}${lastDigit}`);
+  for (let i = 0; i < firstDigitIndex && i < str.length; i++) {
+    const char = str[i];
+    if (isDigit(char)) {
+      firstDigit = char;
+      break;
+    }
+  }
+
+  for (let i = str.length - 1; i > lastDigitIndex && i >= 0; i--) {
+    const char = str[i];
+    if (isDigit(char)) {
+      lastDigit = char;
+      break;
+    }
+  }
+
+  return Number(`${firstDigit}${lastDigit}`);
 }
 
 function isDigit(str: string): boolean {
-    return /^\d$/.test(str);
+  return /^\d$/.test(str);
 }
 
-const input = readFileSync('input.txt', 'utf-8');
+const input = readFileSync("input.txt", "utf-8");
 
-const ans = input.split('\n')
-    .map((str) => calibration(str))
-    .reduce((acc, cur) => acc + cur);
+const ans = input
+  .split("\n")
+  .map((str) => calibration(str))
+  .reduce((acc, cur) => acc + cur);
 
 console.log(ans);
